535ad6ae5fd2dd4a8e02b037cf75f434e3976c74
irfanmurtaza-el <irfan.murtaza@emerald-labs.com>
2023-10-18 18:40:20 +0500

    VectorDBQAChain is replaced with ConversationChain due to chat context issue

--- lib/langchain/chain.ts
+++ lib/langchain/chain.ts
@@ -1,11 +1,12 @@
 // langchain/chain.ts
-import { VectorDBQAChain } from "langchain/chains"
+import { ConversationChain, VectorDBQAChain } from "langchain/chains"
 import { ChatMessageHistory } from "langchain/memory"
 import { mapStoredMessagesToChatMessages } from "@/lib/langchain/schema"
 import { BufferMemory } from "langchain/memory"
 import { BaseChatMessage, ChainValues } from "langchain/schema"
-import { BaseLanguageModel } from "langchain/dist/base_language"
-import { VectorStore } from "langchain/dist/vectorstores/base"
+import { BaseLanguageModel } from "langchain/base_language"
+import { VectorStore } from "langchain/vectorstores/base"
+import { ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate } from "langchain/prompts"
 
 export function getChain(model: BaseLanguageModel, vectorStore: VectorStore, sanitizedQuestion: any, history: BaseChatMessage[]) {
   const lcChatMessageHistory = new ChatMessageHistory(
@@ -17,14 +18,24 @@ export function getChain(model: BaseLanguageModel, vectorStore: VectorStore, san
     memoryKey: "history",
   })
 
-  const chain = VectorDBQAChain.fromLLM(model, vectorStore, {
-    returnSourceDocuments: true,
-  })
+  const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate("You are a friendly assistant."),
+    new MessagesPlaceholder("history"),
+    HumanMessagePromptTemplate.fromTemplate("{input}"),
+  ]);
+  const chain = new ConversationChain({
+    memory: memory,
+    llm: model,
+    prompt: chatPrompt,
+  });
+
+
+  // const chain = VectorDBQAChain.fromLLM(model, vectorStore, {
+  //   returnSourceDocuments: true,
+  // })
 
   const response: string | ChainValues = chain.call({
-    query: sanitizedQuestion,
-    chat_history: history,
-    memory: memory,
+    input: sanitizedQuestion,
   })
 
   return response
