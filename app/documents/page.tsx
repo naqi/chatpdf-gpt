127a021d72d801a8b6c46eddce98a896527412a4
Naqi Syed <naqisyed@gmail.com>
2023-12-04 23:30:35 -0600

    no alert needed

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -26,44 +26,12 @@ export default function Page() {
     fetchDocuments();
   }, [data]);
     // checks if credentials are set
-    const checkCredentials = () => {
-      if (
-        !cookieValue.openaiApiKey ||
-        !cookieValue.pineconeEnvironment ||
-        !cookieValue.pineconeIndex ||
-        !cookieValue.pineconeApiKey ||
-        !cookieValue.supabaseUrl ||
-        !cookieValue.supabaseKey ||
-        !cookieValue.supabaseBucket ||
-        !cookieValue.supabaseDatabaseUrl ||
-        !cookieValue.supabaseDirectUrl
-      ) {
-        return false;
-      }
-      return true;
-    };
 
   return (
     <section className="container  grid h-full items-center gap-6 pb-8 pt-6">
         <h3 className="text-2xl font-bold leading-tight text-gray-900">
             Documents
         </h3>
-        {!checkCredentials() && <Alert variant="warning">
-                  <AlertCircle className="h-4 w-4" />
-                  <AlertDescription>
-                  <div>
-          This app requires you to{" "}
-          <Link
-            className="cursor-pointer text-blue-500 hover:text-blue-700 hover:underline"
-            href="/credentials"
-            rel="noreferrer"
-          >
-            add your credentials
-          </Link>{" "}
-          to work properly.
-        </div>
-                  </AlertDescription>
-                </Alert>}
       <DocumentsTable documents={documents} setDocuments={setDocuments} />
     </section>
   );
