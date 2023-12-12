64d3b25eeeb8f7e119b2f7f7b5e3b2fdf5c72dd4
Naqi Syed <naqisyed@gmail.com>
2023-12-12 13:38:56 -0600

    fixing middleware

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const password = request.nextUrl.searchParams.get(
-    process.env.NEXT_PUBLIC_SEARCH_QUERY_NAME!
-  );
-  const hasCookie = request.cookies.has(process.env.NEXT_PUBLIC_PASSWORD_COOKIE_NAME!);
-  const url = request.nextUrl.clone();
-  const response = NextResponse.redirect(url);
+  if (!request.url.includes("api")) {
+    const password = request.nextUrl.searchParams.get(
+      process.env.NEXT_PUBLIC_SEARCH_QUERY_NAME!
+    );
+    const hasCookie = request.cookies.has(process.env.NEXT_PUBLIC_PASSWORD_COOKIE_NAME!);
+    const url = request.nextUrl.clone();
+    const response = NextResponse.redirect(url);
 
-  if (password === process.env.PAGE_PASSWORD && !hasCookie) {
-    response.cookies.set(`${process.env.NEXT_PUBLIC_PASSWORD_COOKIE_NAME}`, "true");
-    return response;
+    if (password === process.env.PAGE_PASSWORD && !hasCookie) {
+      response.cookies.set(`${process.env.NEXT_PUBLIC_PASSWORD_COOKIE_NAME}`, "true");
+      return response;
+    }
   }
 }
