001fb859118700f7f3afe45b47f5038e29fec066
Naqi Syed <naqisyed@gmail.com>
2024-06-05 16:19:21 -0500

    refactoring code and introducing new supabase vector support

---
+++ utils/credentials.ts
@@ -0,0 +1,11 @@
+export const cookieValue = {
+  openaiApiKey: process.env.OPENAI_API_KEY,
+  pineconeEnvironment: process.env.PINECONE_ENVIRONMENT,
+  pineconeIndex: process.env.PINECONE_INDEX_NAME,
+  pineconeApiKey: process.env.PINECONE_API_KEY,
+  supabaseUrl: process.env.SUPABASE_URL,
+  supabaseKey: process.env.SUPABASE_KEY,
+  supabaseBucket: process.env.SUPABASE_BUCKET,
+  supabaseDatabaseUrl: process.env.DATABASE_URL,
+  supabaseDirectUrl: process.env.DIRECT_URL,
+}
