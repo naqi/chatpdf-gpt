b2d695997bffff6dc0c1f26eb85e2e8618182235
Naqi Syed <naqisyed@gmail.com>
2024-06-06 00:13:21 -0500

    cleaning code

--- app/api/chat-listing/[childId]/route.ts
+++ app/api/chat-listing/[childId]/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: NextRequest, { params: { childId } }) {
 
   const pageNumber = request.nextUrl.searchParams.get("pageNumber") as string
   const pageSize = request.nextUrl.searchParams.get("pageSize") as string
-  
+
   if (pageNumber && pageSize) {
     skip = parseInt(pageSize) * (parseInt(pageNumber) - 1)
     take = parseInt(pageSize)
@@ -18,7 +18,7 @@ export async function GET(request: NextRequest, { params: { childId } }) {
   const credentials = {
     supabaseDatabaseUrl: process.env.NEXT_PUBLIC_DATABASE_URL,
   }
-  const prisma = createPrisma({ url: credentials.supabaseDatabaseUrl })
+  const prisma = createPrisma()
 
   const data = await prisma.chatHistory.findMany({
     skip: skip,
